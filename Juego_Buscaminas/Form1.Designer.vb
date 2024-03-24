c52a49644f4db62694824371592eedb6d5279a31
Alejandra <alejandraalvarezvillarreal2002@gmail.com>
2024-03-24 09:22:35 -0500

    Add project files.

---
+++ Juego_Buscaminas/Form1.Designer.vb
@@ -0,0 +1,572 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        C11 = New PictureBox()
+        C12 = New PictureBox()
+        C13 = New PictureBox()
+        C14 = New PictureBox()
+        C15 = New PictureBox()
+        C16 = New PictureBox()
+        C26 = New PictureBox()
+        C25 = New PictureBox()
+        C24 = New PictureBox()
+        C23 = New PictureBox()
+        C22 = New PictureBox()
+        C21 = New PictureBox()
+        C36 = New PictureBox()
+        C35 = New PictureBox()
+        C34 = New PictureBox()
+        C33 = New PictureBox()
+        C32 = New PictureBox()
+        C31 = New PictureBox()
+        C46 = New PictureBox()
+        C45 = New PictureBox()
+        C44 = New PictureBox()
+        C43 = New PictureBox()
+        C42 = New PictureBox()
+        C41 = New PictureBox()
+        C56 = New PictureBox()
+        C55 = New PictureBox()
+        C54 = New PictureBox()
+        C53 = New PictureBox()
+        C52 = New PictureBox()
+        C51 = New PictureBox()
+        C66 = New PictureBox()
+        C65 = New PictureBox()
+        C64 = New PictureBox()
+        C63 = New PictureBox()
+        C62 = New PictureBox()
+        C61 = New PictureBox()
+        Button1 = New Button()
+        CType(C11, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C12, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C13, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C14, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C15, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C16, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C26, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C25, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C24, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C23, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C22, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C21, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C36, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C35, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C34, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C33, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C32, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C31, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C46, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C45, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C44, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C43, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C42, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C41, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C56, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C55, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C54, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C53, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C52, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C51, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C66, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C65, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C64, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C63, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C62, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C61, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.ButtonFace
+        Label1.Location = New Point(23, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(366, 41)
+        Label1.TabIndex = 0
+        Label1.Text = "JUEGO DE BUSCAMINAS"
+        ' 
+        ' C11
+        ' 
+        C11.Location = New Point(59, 86)
+        C11.Name = "C11"
+        C11.Size = New Size(40, 36)
+        C11.SizeMode = PictureBoxSizeMode.StretchImage
+        C11.TabIndex = 1
+        C11.TabStop = False
+        ' 
+        ' C12
+        ' 
+        C12.Location = New Point(105, 86)
+        C12.Name = "C12"
+        C12.Size = New Size(40, 36)
+        C12.SizeMode = PictureBoxSizeMode.StretchImage
+        C12.TabIndex = 2
+        C12.TabStop = False
+        ' 
+        ' C13
+        ' 
+        C13.Location = New Point(151, 86)
+        C13.Name = "C13"
+        C13.Size = New Size(40, 36)
+        C13.SizeMode = PictureBoxSizeMode.StretchImage
+        C13.TabIndex = 3
+        C13.TabStop = False
+        ' 
+        ' C14
+        ' 
+        C14.Location = New Point(197, 86)
+        C14.Name = "C14"
+        C14.Size = New Size(40, 36)
+        C14.SizeMode = PictureBoxSizeMode.StretchImage
+        C14.TabIndex = 4
+        C14.TabStop = False
+        ' 
+        ' C15
+        ' 
+        C15.Location = New Point(243, 86)
+        C15.Name = "C15"
+        C15.Size = New Size(40, 36)
+        C15.SizeMode = PictureBoxSizeMode.StretchImage
+        C15.TabIndex = 5
+        C15.TabStop = False
+        ' 
+        ' C16
+        ' 
+        C16.Location = New Point(289, 86)
+        C16.Name = "C16"
+        C16.Size = New Size(40, 36)
+        C16.SizeMode = PictureBoxSizeMode.StretchImage
+        C16.TabIndex = 6
+        C16.TabStop = False
+        ' 
+        ' C26
+        ' 
+        C26.Location = New Point(289, 128)
+        C26.Name = "C26"
+        C26.Size = New Size(40, 36)
+        C26.SizeMode = PictureBoxSizeMode.StretchImage
+        C26.TabIndex = 12
+        C26.TabStop = False
+        ' 
+        ' C25
+        ' 
+        C25.Location = New Point(243, 128)
+        C25.Name = "C25"
+        C25.Size = New Size(40, 36)
+        C25.SizeMode = PictureBoxSizeMode.StretchImage
+        C25.TabIndex = 11
+        C25.TabStop = False
+        ' 
+        ' C24
+        ' 
+        C24.Location = New Point(197, 128)
+        C24.Name = "C24"
+        C24.Size = New Size(40, 36)
+        C24.SizeMode = PictureBoxSizeMode.StretchImage
+        C24.TabIndex = 10
+        C24.TabStop = False
+        ' 
+        ' C23
+        ' 
+        C23.Location = New Point(151, 128)
+        C23.Name = "C23"
+        C23.Size = New Size(40, 36)
+        C23.SizeMode = PictureBoxSizeMode.StretchImage
+        C23.TabIndex = 9
+        C23.TabStop = False
+        ' 
+        ' C22
+        ' 
+        C22.Location = New Point(105, 128)
+        C22.Name = "C22"
+        C22.Size = New Size(40, 36)
+        C22.SizeMode = PictureBoxSizeMode.StretchImage
+        C22.TabIndex = 8
+        C22.TabStop = False
+        ' 
+        ' C21
+        ' 
+        C21.Location = New Point(59, 128)
+        C21.Name = "C21"
+        C21.Size = New Size(40, 36)
+        C21.SizeMode = PictureBoxSizeMode.StretchImage
+        C21.TabIndex = 7
+        C21.TabStop = False
+        ' 
+        ' C36
+        ' 
+        C36.Location = New Point(289, 170)
+        C36.Name = "C36"
+        C36.Size = New Size(40, 36)
+        C36.SizeMode = PictureBoxSizeMode.StretchImage
+        C36.TabIndex = 18
+        C36.TabStop = False
+        ' 
+        ' C35
+        ' 
+        C35.Location = New Point(243, 170)
+        C35.Name = "C35"
+        C35.Size = New Size(40, 36)
+        C35.SizeMode = PictureBoxSizeMode.StretchImage
+        C35.TabIndex = 17
+        C35.TabStop = False
+        ' 
+        ' C34
+        ' 
+        C34.Location = New Point(197, 170)
+        C34.Name = "C34"
+        C34.Size = New Size(40, 36)
+        C34.SizeMode = PictureBoxSizeMode.StretchImage
+        C34.TabIndex = 16
+        C34.TabStop = False
+        ' 
+        ' C33
+        ' 
+        C33.Location = New Point(151, 170)
+        C33.Name = "C33"
+        C33.Size = New Size(40, 36)
+        C33.SizeMode = PictureBoxSizeMode.StretchImage
+        C33.TabIndex = 15
+        C33.TabStop = False
+        ' 
+        ' C32
+        ' 
+        C32.Location = New Point(105, 170)
+        C32.Name = "C32"
+        C32.Size = New Size(40, 36)
+        C32.SizeMode = PictureBoxSizeMode.StretchImage
+        C32.TabIndex = 14
+        C32.TabStop = False
+        ' 
+        ' C31
+        ' 
+        C31.Location = New Point(59, 170)
+        C31.Name = "C31"
+        C31.Size = New Size(40, 36)
+        C31.SizeMode = PictureBoxSizeMode.StretchImage
+        C31.TabIndex = 13
+        C31.TabStop = False
+        ' 
+        ' C46
+        ' 
+        C46.Location = New Point(289, 212)
+        C46.Name = "C46"
+        C46.Size = New Size(40, 36)
+        C46.SizeMode = PictureBoxSizeMode.StretchImage
+        C46.TabIndex = 24
+        C46.TabStop = False
+        ' 
+        ' C45
+        ' 
+        C45.Location = New Point(243, 212)
+        C45.Name = "C45"
+        C45.Size = New Size(40, 36)
+        C45.SizeMode = PictureBoxSizeMode.StretchImage
+        C45.TabIndex = 23
+        C45.TabStop = False
+        ' 
+        ' C44
+        ' 
+        C44.Location = New Point(197, 212)
+        C44.Name = "C44"
+        C44.Size = New Size(40, 36)
+        C44.SizeMode = PictureBoxSizeMode.StretchImage
+        C44.TabIndex = 22
+        C44.TabStop = False
+        ' 
+        ' C43
+        ' 
+        C43.Location = New Point(151, 212)
+        C43.Name = "C43"
+        C43.Size = New Size(40, 36)
+        C43.SizeMode = PictureBoxSizeMode.StretchImage
+        C43.TabIndex = 21
+        C43.TabStop = False
+        ' 
+        ' C42
+        ' 
+        C42.Location = New Point(105, 212)
+        C42.Name = "C42"
+        C42.Size = New Size(40, 36)
+        C42.SizeMode = PictureBoxSizeMode.StretchImage
+        C42.TabIndex = 20
+        C42.TabStop = False
+        ' 
+        ' C41
+        ' 
+        C41.Location = New Point(59, 212)
+        C41.Name = "C41"
+        C41.Size = New Size(40, 36)
+        C41.SizeMode = PictureBoxSizeMode.StretchImage
+        C41.TabIndex = 19
+        C41.TabStop = False
+        ' 
+        ' C56
+        ' 
+        C56.Location = New Point(289, 254)
+        C56.Name = "C56"
+        C56.Size = New Size(40, 36)
+        C56.SizeMode = PictureBoxSizeMode.StretchImage
+        C56.TabIndex = 30
+        C56.TabStop = False
+        ' 
+        ' C55
+        ' 
+        C55.Location = New Point(243, 254)
+        C55.Name = "C55"
+        C55.Size = New Size(40, 36)
+        C55.SizeMode = PictureBoxSizeMode.StretchImage
+        C55.TabIndex = 29
+        C55.TabStop = False
+        ' 
+        ' C54
+        ' 
+        C54.Location = New Point(197, 254)
+        C54.Name = "C54"
+        C54.Size = New Size(40, 36)
+        C54.SizeMode = PictureBoxSizeMode.StretchImage
+        C54.TabIndex = 28
+        C54.TabStop = False
+        ' 
+        ' C53
+        ' 
+        C53.Location = New Point(151, 254)
+        C53.Name = "C53"
+        C53.Size = New Size(40, 36)
+        C53.SizeMode = PictureBoxSizeMode.StretchImage
+        C53.TabIndex = 27
+        C53.TabStop = False
+        ' 
+        ' C52
+        ' 
+        C52.Location = New Point(105, 254)
+        C52.Name = "C52"
+        C52.Size = New Size(40, 36)
+        C52.SizeMode = PictureBoxSizeMode.StretchImage
+        C52.TabIndex = 26
+        C52.TabStop = False
+        ' 
+        ' C51
+        ' 
+        C51.Location = New Point(59, 254)
+        C51.Name = "C51"
+        C51.Size = New Size(40, 36)
+        C51.SizeMode = PictureBoxSizeMode.StretchImage
+        C51.TabIndex = 25
+        C51.TabStop = False
+        ' 
+        ' C66
+        ' 
+        C66.Location = New Point(289, 296)
+        C66.Name = "C66"
+        C66.Size = New Size(40, 36)
+        C66.SizeMode = PictureBoxSizeMode.StretchImage
+        C66.TabIndex = 36
+        C66.TabStop = False
+        ' 
+        ' C65
+        ' 
+        C65.Location = New Point(243, 296)
+        C65.Name = "C65"
+        C65.Size = New Size(40, 36)
+        C65.SizeMode = PictureBoxSizeMode.StretchImage
+        C65.TabIndex = 35
+        C65.TabStop = False
+        ' 
+        ' C64
+        ' 
+        C64.Location = New Point(197, 296)
+        C64.Name = "C64"
+        C64.Size = New Size(40, 36)
+        C64.SizeMode = PictureBoxSizeMode.StretchImage
+        C64.TabIndex = 34
+        C64.TabStop = False
+        ' 
+        ' C63
+        ' 
+        C63.Location = New Point(151, 296)
+        C63.Name = "C63"
+        C63.Size = New Size(40, 36)
+        C63.SizeMode = PictureBoxSizeMode.StretchImage
+        C63.TabIndex = 33
+        C63.TabStop = False
+        ' 
+        ' C62
+        ' 
+        C62.Location = New Point(105, 296)
+        C62.Name = "C62"
+        C62.Size = New Size(40, 36)
+        C62.SizeMode = PictureBoxSizeMode.StretchImage
+        C62.TabIndex = 32
+        C62.TabStop = False
+        ' 
+        ' C61
+        ' 
+        C61.Location = New Point(59, 296)
+        C61.Name = "C61"
+        C61.Size = New Size(40, 36)
+        C61.SizeMode = PictureBoxSizeMode.StretchImage
+        C61.TabIndex = 31
+        C61.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Azure
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Location = New Point(134, 357)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(134, 47)
+        Button1.TabIndex = 37
+        Button1.Text = "REINICIAR"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightSlateGray
+        ClientSize = New Size(403, 429)
+        Controls.Add(Button1)
+        Controls.Add(C66)
+        Controls.Add(C65)
+        Controls.Add(C64)
+        Controls.Add(C63)
+        Controls.Add(C62)
+        Controls.Add(C61)
+        Controls.Add(C56)
+        Controls.Add(C55)
+        Controls.Add(C54)
+        Controls.Add(C53)
+        Controls.Add(C52)
+        Controls.Add(C51)
+        Controls.Add(C46)
+        Controls.Add(C45)
+        Controls.Add(C44)
+        Controls.Add(C43)
+        Controls.Add(C42)
+        Controls.Add(C41)
+        Controls.Add(C36)
+        Controls.Add(C35)
+        Controls.Add(C34)
+        Controls.Add(C33)
+        Controls.Add(C32)
+        Controls.Add(C31)
+        Controls.Add(C26)
+        Controls.Add(C25)
+        Controls.Add(C24)
+        Controls.Add(C23)
+        Controls.Add(C22)
+        Controls.Add(C21)
+        Controls.Add(C16)
+        Controls.Add(C15)
+        Controls.Add(C14)
+        Controls.Add(C13)
+        Controls.Add(C12)
+        Controls.Add(C11)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Buscaminas"
+        CType(C11, ComponentModel.ISupportInitialize).EndInit()
+        CType(C12, ComponentModel.ISupportInitialize).EndInit()
+        CType(C13, ComponentModel.ISupportInitialize).EndInit()
+        CType(C14, ComponentModel.ISupportInitialize).EndInit()
+        CType(C15, ComponentModel.ISupportInitialize).EndInit()
+        CType(C16, ComponentModel.ISupportInitialize).EndInit()
+        CType(C26, ComponentModel.ISupportInitialize).EndInit()
+        CType(C25, ComponentModel.ISupportInitialize).EndInit()
+        CType(C24, ComponentModel.ISupportInitialize).EndInit()
+        CType(C23, ComponentModel.ISupportInitialize).EndInit()
+        CType(C22, ComponentModel.ISupportInitialize).EndInit()
+        CType(C21, ComponentModel.ISupportInitialize).EndInit()
+        CType(C36, ComponentModel.ISupportInitialize).EndInit()
+        CType(C35, ComponentModel.ISupportInitialize).EndInit()
+        CType(C34, ComponentModel.ISupportInitialize).EndInit()
+        CType(C33, ComponentModel.ISupportInitialize).EndInit()
+        CType(C32, ComponentModel.ISupportInitialize).EndInit()
+        CType(C31, ComponentModel.ISupportInitialize).EndInit()
+        CType(C46, ComponentModel.ISupportInitialize).EndInit()
+        CType(C45, ComponentModel.ISupportInitialize).EndInit()
+        CType(C44, ComponentModel.ISupportInitialize).EndInit()
+        CType(C43, ComponentModel.ISupportInitialize).EndInit()
+        CType(C42, ComponentModel.ISupportInitialize).EndInit()
+        CType(C41, ComponentModel.ISupportInitialize).EndInit()
+        CType(C56, ComponentModel.ISupportInitialize).EndInit()
+        CType(C55, ComponentModel.ISupportInitialize).EndInit()
+        CType(C54, ComponentModel.ISupportInitialize).EndInit()
+        CType(C53, ComponentModel.ISupportInitialize).EndInit()
+        CType(C52, ComponentModel.ISupportInitialize).EndInit()
+        CType(C51, ComponentModel.ISupportInitialize).EndInit()
+        CType(C66, ComponentModel.ISupportInitialize).EndInit()
+        CType(C65, ComponentModel.ISupportInitialize).EndInit()
+        CType(C64, ComponentModel.ISupportInitialize).EndInit()
+        CType(C63, ComponentModel.ISupportInitialize).EndInit()
+        CType(C62, ComponentModel.ISupportInitialize).EndInit()
+        CType(C61, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents C11 As PictureBox
+    Friend WithEvents C12 As PictureBox
+    Friend WithEvents C13 As PictureBox
+    Friend WithEvents C14 As PictureBox
+    Friend WithEvents C15 As PictureBox
+    Friend WithEvents C16 As PictureBox
+    Friend WithEvents C26 As PictureBox
+    Friend WithEvents C25 As PictureBox
+    Friend WithEvents C24 As PictureBox
+    Friend WithEvents C23 As PictureBox
+    Friend WithEvents C22 As PictureBox
+    Friend WithEvents C21 As PictureBox
+    Friend WithEvents C36 As PictureBox
+    Friend WithEvents C35 As PictureBox
+    Friend WithEvents C34 As PictureBox
+    Friend WithEvents C33 As PictureBox
+    Friend WithEvents C32 As PictureBox
+    Friend WithEvents C31 As PictureBox
+    Friend WithEvents C46 As PictureBox
+    Friend WithEvents C45 As PictureBox
+    Friend WithEvents C44 As PictureBox
+    Friend WithEvents C43 As PictureBox
+    Friend WithEvents C42 As PictureBox
+    Friend WithEvents C41 As PictureBox
+    Friend WithEvents C56 As PictureBox
+    Friend WithEvents C55 As PictureBox
+    Friend WithEvents C54 As PictureBox
+    Friend WithEvents C53 As PictureBox
+    Friend WithEvents C52 As PictureBox
+    Friend WithEvents C51 As PictureBox
+    Friend WithEvents C66 As PictureBox
+    Friend WithEvents C65 As PictureBox
+    Friend WithEvents C64 As PictureBox
+    Friend WithEvents C63 As PictureBox
+    Friend WithEvents C62 As PictureBox
+    Friend WithEvents C61 As PictureBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Juego_Buscaminas/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Dim V(36) As Integer
+    Dim I, N As Integer
+
+    Sub REINICIAR()
+        Dim OBJ As Control
+
+        For Each OBJ In Me.Controls
+            If TypeOf OBJ Is PictureBox Then
+                Dim IMG As PictureBox
+                OBJ.Tag = ""
+                IMG = OBJ
+                IMG.Image = Nothing
+                IMG.Enabled = True
+            End If
+            I = 0
+            N = 0
+        Next
+    End Sub
+
+    Sub aleatorio()
+        Randomize()
+        Dim VAR, C As Integer
+        VAR = 1
+        While VAR > 0
+            VAR = 0
+            I = Math.Truncate(Rnd() * 36 + 1) 'genera un numero aletario en un rango de 1-36)
+            For C = 0 To N
+                If V(C) = I Then
+                    VAR += 1
+                End If
+            Next
+        End While
+        V(N) = I
+        N += 1
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub C11_Click(sender As Object, e As EventArgs) Handles C11.Click, C12.Click, C13.Click, C14.Click, C15.Click, C16.Click, C21.Click, C22.Click, C23.Click, C24.Click, C25.Click, C26.Click, C31.Click, C32.Click, C33.Click, C34.Click, C35.Click, C36.Click, C41.Click, C42.Click, C43.Click, C44.Click, C45.Click, C46.Click, C51.Click, C52.Click, C53.Click, C54.Click, C55.Click, C56.Click, C61.Click, C62.Click, C63.Click, C64.Click, C65.Click, C66.Click
+        aleatorio()
+        sender.tag = CStr(I)
+        If I = 1 Or I = 2 Or I = 4 Or I = 5 Or I = 6 Then
+            sender.image = Image.FromFile("C:\Users\LolaA\source\repos\Juego_Buscaminas\Juego_Buscaminas\Image\mina.png")
+            MsgBox("MINA, PERDISTE", MsgBoxStyle.Critical, "DESACIERTO")
+            Dim OBJ As Control
+            For Each OBJ In Me.Controls
+                If TypeOf OBJ Is PictureBox And OBJ.Tag = "" Then
+                    aleatorio()
+                    If I = 1 Or I = 2 Or I = 4 Or I = 5 Or I = 6 Then
+                        Dim imagen As PictureBox
+                        imagen = OBJ
+                        imagen.Image = Image.FromFile("C:\Users\LolaA\source\repos\Juego_Buscaminas\Juego_Buscaminas\Image\mina.png")
+                    End If
+                    OBJ.Tag = CStr(I)
+                End If
+            Next
+
+        Else
+            sender.image = Image.FromFile("C:\Users\LolaA\source\repos\Juego_Buscaminas\Juego_Buscaminas\Image\carta1.png")
+            If N = 30 Then
+                MsgBox("BIEN HECHO, GANASTE!!!", MsgBoxStyle.Information, "ACIERTO")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        REINICIAR()
+    End Sub
+End Class
